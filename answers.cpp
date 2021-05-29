e87e5ef20f1fb3877c99d64ebc15051b04f64fd9
Shubham <tiwarishubham635@gmail.com>
2021-05-29 19:39:44 +0530

    Update answers.cpp

--- answers.cpp
+++ answers.cpp
@@ -3487,3 +3487,34 @@ int maxStackHeight( Box arr[], int n )
 	        mx = max(mx, dp[i]);
 	    return mx;
 	}
+87.
+	class MinStack 
+	{
+	    stack<int>s1;
+	    stack<int>s2;
+	public:
+	    /** initialize your data structure here. */
+	    MinStack() {
+
+	    }
+
+	    void push(int val) {
+		s1.push(val);
+		if(s2.empty()||s2.top()>=val)
+		    s2.push(val);
+	    }
+
+	    void pop() {
+		if(s1.top()==s2.top())
+		    s2.pop();
+		s1.pop();
+	    }
+
+	    int top() {
+		return s1.top();
+	    }
+
+	    int getMin() {
+		return s2.top();
+	    }
+	};
