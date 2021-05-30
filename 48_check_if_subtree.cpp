c12a7a12e455ddac194ef6412bf958200701b366
Shubham <tiwarishubham635@gmail.com>
2021-05-30 21:04:54 +0530

    Create 48_check_if_subtree.cpp

---
+++ 48_check_if_subtree.cpp
@@ -0,0 +1,97 @@
+Approach-1:
+bool areIdentical(node * root1, node *root2) 
+{ 
+    /* base cases */
+    if (root1 == NULL && root2 == NULL) 
+        return true; 
+  
+    if (root1 == NULL || root2 == NULL) 
+        return false; 
+  
+    /* Check if the data of both roots is 
+    same and data of left and right 
+    subtrees are also same */
+    return (root1->data == root2->data && 
+            areIdentical(root1->left, root2->left) && 
+            areIdentical(root1->right, root2->right) ); 
+} 
+  
+  
+/* This function returns true if S 
+is a subtree of T, otherwise false */
+bool isSubtree(node *T, node *S) 
+{ 
+    /* base cases */
+    if (S == NULL) 
+        return true; 
+  
+    if (T == NULL) 
+        return false; 
+  
+    /* Check the tree with root as current node */
+    if (areIdentical(T, S)) 
+        return true; 
+  
+    /* If the tree with root as current 
+    node doesn't match then try left 
+    and right subtrees one by one */
+    return isSubtree(T->left, S) || 
+        isSubtree(T->right, S); 
+} 
+
+Approach-2:
+void storeInorder(Node* root, char arr[], int& i)
+{
+    if (root == NULL) {
+        arr[i++] = '$';
+        return;
+    }
+    storeInorder(root->left, arr, i);
+    arr[i++] = root->data;
+    storeInorder(root->right, arr, i);
+}
+
+// A utility function to store preorder traversal of tree rooted
+// with root in an array arr[]. Note that i is passed as reference
+void storePreOrder(Node* root, char arr[], int& i)
+{
+    if (root == NULL) {
+        arr[i++] = '$';
+        return;
+    }
+    arr[i++] = root->data;
+    storePreOrder(root->left, arr, i);
+    storePreOrder(root->right, arr, i);
+}
+
+bool isSubTree(Node* T, Node* S)
+{
+    if (S == NULL)
+    return true;
+    if (T == NULL)
+        return false;
+
+    // Store Inorder traversals of T and S in inT[0..m-1]
+    // and inS[0..n-1] respectively
+    int m = 0, n = 0;
+    char inT[MAX], inS[MAX];
+    storeInorder(T, inT, m);
+    storeInorder(S, inS, n);
+    inT[m] = '\0', inS[n] = '\0';
+
+    // If inS[] is not a substring of inT[], return false
+    if (strstr(inT, inS) == NULL)
+        return false;
+
+    // Store Preorder traversals of T and S in preT[0..m-1]
+    // and preS[0..n-1] respectively
+    m = 0, n = 0;
+    char preT[MAX], preS[MAX];
+    storePreOrder(T, preT, m);
+    storePreOrder(S, preS, n);
+    preT[m] = '\0', preS[n] = '\0';
+
+    // If preS[] is not a substring of preT[], return false
+    // Else return true
+    return (strstr(preT, preS) != NULL);
+}
