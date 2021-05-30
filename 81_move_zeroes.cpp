2e56e9fe429c990e9a04a89cfabc9db4fe10a5e8
Shubham <tiwarishubham635@gmail.com>
2021-05-30 22:18:28 +0530

    Create 81_move_zeroes.cpp

---
+++ 81_move_zeroes.cpp
@@ -0,0 +1,20 @@
+void moveZeroes(vector<int>& nums) 
+    {
+        int n = nums.size(); int j=0;
+        /*for(int i=0;i<n;i++)
+        {
+            if(nums[i]!=0)
+                nums[j++]=nums[i];
+        }
+        for(;j<n;j++)
+            nums[j]=0;*/
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==0)
+                j++;
+            else
+            {
+                swap(nums[i-j], nums[i]);
+            }
+        }
+    }
