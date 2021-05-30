b7aea4ec0634b81419c963582fd8d44b0e17e127
Shubham <tiwarishubham635@gmail.com>
2021-05-30 21:38:18 +0530

    Create 51_print_binary_tree_in_vertical_order.cpp

---
+++ 51_print_binary_tree_in_vertical_order.cpp
@@ -0,0 +1,35 @@
+vector<int> verticalOrder(Node *root)
+    {
+        vector<int>tmp;
+        if (!root)
+            return tmp;
+
+        map<int, vector<int> > m;
+        int hd = 0;
+        
+        queue<pair<Node*, int> > que;
+        que.push(make_pair(root, hd));
+    
+        while (!que.empty()) 
+        {
+            pair<Node*, int> temp = que.front();
+            que.pop();
+            hd = temp.second;
+            Node* node = temp.first;
+    
+            m[hd].push_back(node->data);
+    
+            if (node->left != NULL)
+                que.push(make_pair(node->left, hd - 1));
+            if (node->right != NULL)
+                que.push(make_pair(node->right, hd + 1));
+        }
+    
+        map<int, vector<int> >::iterator it;
+        for (it = m.begin(); it != m.end(); it++) 
+        {
+            for (int i = 0; i < it->second.size(); ++i)
+                tmp.push_back(it->second[i]);
+        }
+        return tmp;
+    }
