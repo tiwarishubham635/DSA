e50ebc8b4396ef90ea4e74a00892ebdea433eeb5
Shubham <tiwarishubham635@gmail.com>
2021-05-31 11:57:38 +0530

    Create 91_shortes_common_supersequence.cpp

---
+++ 91_shortes_common_supersequence.cpp
@@ -0,0 +1,49 @@
+int shortestCommonSupersequence(char* X, char* Y, int n, int m)
+    {
+        int** dp = new int*[n+1];
+        /*for(int i=0;i<=n;i++)
+        {
+            dp[i]=new int[m+1];
+            dp[i][0]=0;
+            for(int j=0;j<=m;j++)
+                dp[0][j]=0;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(X[n-i]==Y[m-j])
+                {
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return m+n-dp[n][m];*/
+        
+        for(int i=0;i<=n;i++)
+        {
+            dp[i]=new int[m+1];
+            dp[i][0]=i;
+            for(int j=0;j<=m;j++)
+                dp[0][j]=j;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(X[n-i]==Y[m-j])
+                {
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j] = 1+min(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
