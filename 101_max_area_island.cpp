e09a31567fc6ff411f3ed2e3de73bd17f377ac09
Shubham <tiwarishubham635@gmail.com>
2021-06-01 23:22:08 +0530

    Create 101_max_area_island.cpp

---
+++ 101_max_area_island.cpp
@@ -0,0 +1,20 @@
+int AreaOfIsland(vector<vector<int>>& grid, int i, int j)
+    {
+        if( i >= 0 && i < grid.size() && j >= 0 && j < grid[0].size() && grid[i][j])
+        {
+            grid[i][j] = 0;
+            return 1 + AreaOfIsland(grid, i+1, j) + AreaOfIsland(grid, i-1, j) 
+                     + AreaOfIsland(grid, i, j-1) + AreaOfIsland(grid, i, j+1);
+        }
+        return 0;
+    }
+    
+    int maxAreaOfIsland(vector<vector<int>>& grid) 
+    {
+        int max_area = 0;
+        for(int i = 0; i < grid.size(); i++)
+            for(int j = 0; j < grid[i].size(); j++)
+                if(grid[i][j])
+                    max_area = max(max_area, AreaOfIsland(grid, i, j));
+        return max_area;
+    }
