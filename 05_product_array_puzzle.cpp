bf81c74b6ad5a264d08473fd2edfbdf534123fb1
Shubham <tiwarishubham635@gmail.com>
2021-06-12 22:38:42 +0530

    Update 05_product_array_puzzle.cpp

--- 05_product_array_puzzle.cpp
+++ 05_product_array_puzzle.cpp
@@ -12,3 +12,21 @@ void productPuzzle(int a[], int n)
                                 sum - log10(a[i])))
              << " ";
 }
+
+vector<int> productExceptSelf(vector<int>& nums) 
+    {
+        int n = nums.size();
+        int left=1;
+        int right =1;
+        vector<int>ans(n,1);
+        
+        for(int i=0;i<n;i++)
+        {
+            ans[i]*=left;
+            left*=nums[i];
+            ans[n-1-i]*=right;
+            right*=nums[n-1-i];
+        }
+        
+        return ans;
+    }
