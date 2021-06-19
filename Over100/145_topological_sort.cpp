8f0cb40493319fe182d55b0be99e9dc830a5a27b
Shubham <tiwarishubham635@gmail.com>
2021-06-19 10:30:05 +0530

    Update 145_topological_sort.cpp

--- Over100/145_topological_sort.cpp
+++ Over100/145_topological_sort.cpp
@@ -9,9 +9,9 @@ void topsort(vector<int>adj[], int i, int n, vector<int>&visited, stack<int>&st)
 	    
 	    st.push(i);
 	}
-	vector<int> topoSort(int n, vector<int> adj[]) 
+vector<int> topoSort(int n, vector<int> adj[]) 
 	{
-	    stack<int>st;
+	    /*stack<int>st;
 	    vector<int>visited(n,0);
 	    
 	    for(int i=0;i<n;i++)
@@ -23,5 +23,36 @@ void topsort(vector<int>adj[], int i, int n, vector<int>&visited, stack<int>&st)
 	        ans.push_back(st.top());
 	        st.pop();
 	    }
-	    return ans;
+	    return ans;*/
+	    vector<int>in_degree(n,0);
+	    for(int i=0;i<n;i++)
+	    {
+	        for(int j=0;j<adj[i].size();j++)
+	            in_degree[adj[i][j]]++;
+	    }
+	    
+	    int count=0;
+	    queue<int>q;
+	    for(int i=0;i<n;i++)
+	        if(in_degree[i]==0)
+	            q.push(i);
+	            
+	    vector<int>top_sort;
+	    while(q.size())
+	    {
+	        int node = q.front();
+	        q.pop();
+	        top_sort.push_back(node);
+	        count++;
+	        for(int i=0;i<adj[node].size();i++)
+	        {
+	            in_degree[adj[node][i]]--;
+	            if(in_degree[adj[node][i]]==0)
+	                q.push(adj[node][i]);
+	        }
+	    }
+	    if(count!=n)
+	        top_sort.clear();
+	    
+	    return top_sort;
 	}
