b48178c68e9602c06db79cb969f05b1bfe20f6b5
Shubham <tiwarishubham635@gmail.com>
2021-06-24 11:26:47 +0530

    Update 07_Cycle_Detection.cpp

--- 07_Cycle_Detection.cpp
+++ 07_Cycle_Detection.cpp
@@ -1,3 +1,38 @@
+//Directed
+	
+	bool cycle(vector<int> adj[], int n, int v, vector<int>&visited, vector<int>&rec_stack)
+    {
+        if(!visited[v])
+        {
+            visited[v]=1;
+            rec_stack[v]=1;
+            
+            for(int i=0;i<adj[v].size();i++)
+            {
+                if(!visited[adj[v][i]] && cycle(adj, n, adj[v][i], visited, rec_stack))
+                    return true;
+                else if(rec_stack[adj[v][i]])
+                    return true;
+            }
+        }
+        rec_stack[v]=0;
+        return false;
+    }
+	
+	bool isCyclic(int n, vector<int> adj[]) 
+	{
+	    vector<int>visited(n,0);
+	    vector<int>rec_stack(n,0);
+	    
+	    for(int i=0;i<n;i++)
+	        if(cycle(adj, n, i, visited, rec_stack))
+	            return true;
+	   return false;
+	}
+	
+	// Undirected
+
+
 #include <iostream>
 using namespace std;
 
