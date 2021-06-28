1a6faaf9d953ac58a20a6456283ed98d3e7077a3
Shubham <tiwarishubham635@gmail.com>
2021-06-28 10:26:26 +0530

    Update 161_wonderful_strings.cpp

--- Over100/161_wonderful_strings.cpp
+++ Over100/161_wonderful_strings.cpp
@@ -1,14 +1,23 @@
-long long wonderfulSubstrings(string word) {
-        long long res = 0;
-        vector<int> cnt(1024);
-        int x = 0;
-        cnt[0] = 1;
-        for (char c : word) {
-            x ^= (1<<(c-'a'));
-            res = res + cnt[x];
-            cnt[x] += 1;
+long long wonderfulSubstrings(string word) { // word = aabb
+        long long ans = 0; 
+        vector<int> no_of_times_seen_this_mask(1024,0);
+        int mask = 0;
+        no_of_times_seen_this_mask[0] = 1;
+
+        for (char c : word) 
+        {  
+            mask ^= (1<<(c-'a')); 
+            
+            ans += no_of_times_seen_this_mask[mask]; // previously, each time we have seen this mask, from that index to this index, 
+                                                  // there is a wonderful string having all characters with even frequency 
+            
+            no_of_times_seen_this_mask[mask]++;  // mask is seen this time
+            
             for (int k=0;k<10;++k)
-                res = res + cnt[x^(1<<k)];
+            {
+                int change_of_one_place = mask^(1<<k); // flicking one position in mask, to check odd at exactly one position 
+                ans += no_of_times_seen_this_mask[change_of_one_place]; 
+            }
         }
-        return res;
+        return ans;
     }
