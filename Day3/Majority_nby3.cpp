c1a571b5e0a503d17d9885bfe4ba82d2e8301f2f
Shubham <tiwarishubham635@gmail.com>
2022-01-31 20:13:58 +0530

    Create Majority_nby3.cpp

---
+++ Day3/Majority_nby3.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        vector<int>ans;
+        int num1=-1, num2=-1, count1=0,count2=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(num1 == nums[i])
+            {
+                count1++;
+            }
+            else if(num2==nums[i])
+            {
+                count2++;
+            }
+            else if(count1 == 0)
+            {
+                num1 = nums[i];
+                count1 = 1;
+            }
+            else if(count2 == 0)
+            {
+                num2 = nums[i];
+                count2 = 1;
+            }
+            else
+            {
+                count1--;
+                count2--;
+            }
+        }
+        
+        count1=0, count2=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(num1 == nums[i])
+                count1++;
+            else if(num2 == nums[i])
+                count2++;
+        }
+        if(count1 > nums.size()/3)
+            ans.push_back(num1);
+        if(count2 > nums.size()/3)
+            ans.push_back(num2);
+        return ans;
+    }
+};
